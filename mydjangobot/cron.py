4fb02b02b06fad3f5028a3dd41684d5866cb0dc7
linkeriyo <linkeriyo@gmail.com>
2024-01-28 23:06:14 +0100

    Add date check in cron job

--- mydjangobot/cron.py
+++ mydjangobot/cron.py
@@ -2,6 +2,7 @@ import discord
 
 from mydjangobot import settings
 from mydjangobot import discordbot_settings
+from datetime import datetime, timedelta
 
 client = discord.Client(intents=discord.Intents.default())
 
@@ -9,7 +10,16 @@ client = discord.Client(intents=discord.Intents.default())
 async def on_ready():
     print("discord client ready")
     channel = client.get_channel(settings.MICROSOFT_ACCOUNT_CHANNEL_ID)
-    await channel.send('test')
+    await channel.send('@here pagad el gamepass 1.70€, gracias')
+    await client.close()
+
 
 def scheduled_job():
-    client.run(discordbot_settings.token)
+    if date_check():
+        client.run(discordbot_settings.token)
+
+
+def date_check():
+    today = datetime.now()
+    tomorrow = today + timedelta(days=1)
+    return today.month == 2 and today.month != tomorrow.month or today.day == 30
